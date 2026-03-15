8477a653399eb517fe2bd1427386364cc6a6dbde
Al Wold <alwold@alwold.com>
2026-03-15 08:02:38 -0700

    Fix error when typing ctrl-alt on pattens tab

--- src/neil/utils.py
+++ src/neil/utils.py
@@ -1278,7 +1278,7 @@ class AcceleratorMap:
                 if successful.
                 """
                 # remove numlock from the key modifiers
-                key_mod = event.state & (~Gdk.ModifierType.MOD2_MASK)
+                key_mod = Gdk.ModifierType(event.state & (~Gdk.ModifierType.MOD2_MASK))
                 name = Gtk.accelerator_name(event.keyval, key_mod)
                 if name == None:
                         return False
